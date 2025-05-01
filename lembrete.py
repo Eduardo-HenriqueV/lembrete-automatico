928ec2a6059d566e46c9af6fc7ce05b9b56ec389
Eduardo-HenriqueV <dudu_sccp_lhp_1910@hotmail.com>
2025-05-01 13:29:17 -0300

    Primeiro commit: script de lembrete automático em Python

---
+++ lembrete.py
@@ -0,0 +1,9 @@
+import time
+
+espera_em_segundos = 15
+
+print(f'Aguardando {espera_em_segundos} segundos para enviar a mensagem...')
+
+time.sleep(espera_em_segundos)
+
+print(' Lembrete: A mensagem aguardada chegou!')
